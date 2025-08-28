c92c57e5c04dc63d6b0413aa02d31a3458007d9d
SkyDevF <thitiwut.pr@rmuti.ac.th>
2025-08-28 11:23:08 +0700

    commit 1

---
+++ lib/screens/todo_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-Do'),
+      ),
+      body: Center(
+        child: WidgetsApp(color: Colors.deepPurple),
+      ),
+    );
+  }
+}
